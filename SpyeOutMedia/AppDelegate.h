517641e1133e9d0416efde030a774a17eeff9fae
Husein Kareem <hkareem@Huseins-MBP.local>
2015-08-06 15:30:32 -0400

    Initial Commit

---
+++ SpyeOutMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpyeOutMedia
+//
+//  Created by Husein Kareem on 8/6/15.
+//  Copyright (c) 2015 Husein Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
